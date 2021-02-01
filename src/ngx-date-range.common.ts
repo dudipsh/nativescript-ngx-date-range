386c14ab1a45f029e4d4b8354434a58a0bd2f3c1
Abhijit Dey <57581122+InocentumAbhijit@users.noreply.github.com>
2021-02-01 15:44:47 +0530

    Update ngx-date-range.common.ts

--- src/ngx-date-range.common.ts
+++ src/ngx-date-range.common.ts
@@ -1,30 +1,31 @@
-import { View} from "tns-core-modules/ui/core/view";
+import { View } from "tns-core-modules/ui/core/view";
 
-export class Common extends View {
-
-}
+export class Common extends View {}
 export class Options {
-    selectionMode?: 'SINGLE' | 'MULTIPLE' | 'RANGE';
-    initialDate?: string;
-    simpleDateFormat?: string;
-    supportsRtl?: boolean;
-    disablePrevDates?: boolean;
-    selectToday?: boolean;
-    language: {
-        languageCode: string,
-        countryCode: string
+  selectionMode?: "SINGLE" | "MULTIPLE" | "RANGE";
+  initialDate?: string;
+  simpleDateFormat?: string;
+  supportsRtl?: boolean;
+  disablePrevDates?: boolean;
+  selectToday?: boolean;
+  minDate?: string;
+  maxDate?: string;
+  language: {
+    languageCode: string;
+    countryCode: string;
+  };
+  constructor() {
+    this.selectionMode = "RANGE";
+    this.simpleDateFormat = "MMMM, YYYY";
+    this.initialDate = null;
+    this.supportsRtl = false;
+    this.disablePrevDates = false;
+    this.selectToday = false;
+    this.minDate = null;
+    this.maxDate = null;
+    this.language = {
+      languageCode: null,
+      countryCode: null,
     };
-    constructor() {
-        this.selectionMode = 'RANGE';
-        this.simpleDateFormat = 'MMMM, YYYY';
-        this.initialDate = null;
-        this.supportsRtl = false;
-        this.disablePrevDates = false;
-        this.selectToday = false;
-        this.language = {
-            languageCode: null,
-            countryCode: null
-        };
-    }
+  }
 }
-
